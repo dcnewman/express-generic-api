0b45286bcb77835061429814fba54b81b1043c10
dcnewman <dan.c.newman@icloud.com>
2018-03-13 16:40:26 -0700

    Remove eslint comment

--- server/api/v1/hello/hello.controller.js
+++ server/api/v1/hello/hello.controller.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// eslint-disable-next-line no-unused-vars
-export function helloWorld(req, res, next) {
+export function helloWorld(req, res) {
   return res.status(200).json({msg: 'Hello World!'});
 }
