8603dcd14b640cc2590aa4ad3e38933967858bc7
dcnewman <dan.c.newman@icloud.com>
2018-03-14 20:33:52 -0700

    Some test code

--- server/api/v1/hello/hello.controller.js
+++ server/api/v1/hello/hello.controller.js
@@ -1,5 +1,12 @@
 'use strict';
 
+import Sample from '../../../db_models/sample.model';
+import * as lib from '../../../lib';
+
 export function helloWorld(req, res) {
-  return res.status(200).json({msg: 'Hello World!'});
+  return Sample.create({name: 'Dan', name_s: 'dan'})
+    .then(() => {
+      return res.status(200).json({msg: 'Hello World!'});
+    })
+    .catch(lib.validationError(res));
 }
